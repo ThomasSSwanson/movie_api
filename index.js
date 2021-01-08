fab90e2a07450a2354f9376a4407ba7f05c01a12
Thomas <thomassswanson@gmail.com>
2021-01-07 19:31:07 -0500

    mongoose schema and API routing

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
 const express = require("express"),
-    morgan = require("morgan");
+  morgan = require("morgan");
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // terminal logger
 app.use(morgan('common'));
@@ -16,58 +24,6 @@ app.use((err, req, res, next) => {
 });
 
 
-let topMovies = [
-    {
-      title: 'The Lost Boys',
-      director: 'Joel Shumacher',
-      year: '1987'
-    },
-    {
-      title: 'Day of the Dead',
-      director: 'George A. Romero',
-      year: '1985'
-    },
-    {
-      title: 'Return of the Living Dead',
-      director: 'Dan O\'Bannon',
-      year: '1985'
-    },
-    {
-      title: 'They Live',
-      director:'John Carpenter',
-      year: '1988'
-    },
-    {
-      title: 'Fright Night',
-      director:'Tom Holland',
-      year: '1985'
-    },
-    {
-      title: 'Videodrome',
-      director:'David Cronenberg',
-      year: '1983'
-    },
-    {
-      title: 'Beetlejuice',
-      director:'Tim Burton',
-      year: '1988'
-    },
-    {
-      title: 'The Blob',
-      director:'Chuck Russell',
-      year: '1988'
-    },
-    {
-      title: 'Dreamscape',
-      director:'Joseph Ruben',
-      year: '1984'
-    },
-    {
-      title: 'Poltergeist',
-      director:'Tobe Hooper',
-      year: '1982'
-    }
-  ];
 
 
 app.get('/movies', (req, res) => {
@@ -86,16 +42,112 @@ app.get('/movies/directors/:name', (req, res) => {
   res.send('Successful GET request returning data about a director by name');
 });
 
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  username: String,
+  password: String,
+  email: String,
+  birthday: Date
+}*/
+
 app.post('/users', (req, res) => {
-  res.send('Successful POST request allowing new user to register');
+  Users.findOne({ username: req.body.username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.username + 'already exists');
+      } else {
+        Users
+          .create({
+            username: req.body.username,
+            password: req.body.uassword,
+            email: req.body.email,
+            birthday: req.body.birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
+// Get all users
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Get a user by username
+app.get('/users/:username', (req, res) => {
+  Users.findOne({ username: req.params.username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Update a user's info, by username
+/* We’ll expect JSON in this format
+{
+  username: String,
+  (required)
+  password: String,
+  (required)
+  email: String,
+  (required)
+  birthday: Date
+}*/
 app.put('/users/:username', (req, res) => {
-  res.send('Successful PUT request allowing existing user to update their information');
+  Users.findOneAndUpdate({ Username: req.params.username }, {
+    $set:
+    {
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      birthday: req.body.birthday
+    }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
 });
 
-app.post('/users/:username/movies/:title', (req, res) => {
-  res.send('Successful POST request allowing user to add a movie to their favorites list');
+// Add a movie to a user's list of favorites
+app.post('/users/:username/movies/:movieID', (req, res) => {
+  Users.findOneAndUpdate({ username: req.params.username }, {
+    $push: { favoriteMovies: req.params.movieID }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
 });
 
 app.delete('/users/:username/movies/:title', (req, res) => {
