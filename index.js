5b345e735682f1d65a71b2175a549cb6608dd1bb
Thomas <thomassswanson@gmail.com>
2021-02-21 20:53:36 -0500

    changed connection uri

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ app.use((err, req, res, next) => {
 });
 
 // connecting app to our db using mongoose
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.get('/', (req, res) => {
   res.send('Temp home page');
